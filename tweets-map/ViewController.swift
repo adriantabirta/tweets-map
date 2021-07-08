2541a915babcf51751b878419ad5bb8fe65bba52
Adrian Tabirta <adriantabirta@gmail.com>
2021-07-08 15:29:26 +0300

    Initial Commit

---
+++ tweets-map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tweets-map
+//
+//  Created by Tabirta Adrian on 08.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
